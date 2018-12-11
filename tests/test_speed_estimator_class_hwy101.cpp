982ba05ae0e98b46cf8935b5c586bba526c128f8
djp42 <djp42@stanford.edu>
2018-12-11 14:11:57 -0800

    test speed estimator with time. next stop, making a boost library.

--- tests/test_speed_estimator_class_hwy101.cpp
+++ tests/test_speed_estimator_class_hwy101.cpp
@@ -35,9 +35,13 @@ int main(int argc, char const *argv[]) {
         cout<<"speed_reading: "<<speed_reading<<endl;
     }
     */
+    // First, we do not force fps, just to make sure it still works.
     clock_t start = clock();
-    for (int image_number=2700; image_number<=2800 ;image_number++) {
+    for (int image_number=2700; image_number<=2750 ;image_number++) {
         double cur_time = double(clock() - start) / CLOCKS_PER_SEC;
+        if (image_number == 2700) {
+            cur_time = 0.0;
+        }
         cout << cur_time << endl;
         string file_name = folder_name+to_string(image_number)+".jpg";
         Mat img3 = imread(file_name.c_str());
@@ -46,5 +50,21 @@ int main(int argc, char const *argv[]) {
         cout<<"speed_reading: "<<speed_reading<<endl;
     }
     cout << "took " <<  double(clock() - start) / CLOCKS_PER_SEC << " seconds." << endl;
+    // Next, we want to check that if we force fps, since the videos were recorded
+    // at a different time than we are running this, let's make sure it gets the
+    // correct-ish value
+    const double fps = 30.0;
+    double curtime = 0.0;
+    Speed_estimator speed_est2 = Speed_estimator();
+    for (int image_number=2700; image_number<=2800 ;image_number++) {
+        curtime += 1.0/fps;
+        cout << curtime << endl;
+        string file_name = folder_name+to_string(image_number)+".jpg";
+        Mat img3 = imread(file_name.c_str());
+        speed_est2.Speed_estimator_update(img3, curtime);
+        speed_reading = speed_est2.get_speed();
+        cout<<"speed_reading: "<<speed_reading<<endl;
+    }
+    assert(int(speed_reading) == 37);
     return 0;
 }
