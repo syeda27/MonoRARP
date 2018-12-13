626460c4ee4936d8c30b4ee6a75a34b612d33845
djp42 <djp42@stanford.edu>
2018-12-12 18:27:23 -0800

    increased risk horizon

--- run_risk_prediction.sh
+++ run_risk_prediction.sh
@@ -68,7 +68,7 @@ RELATIVE_HORIZON=0.5         # between 0 and 1
 DETECT_LANES='false'
 
 RISK_N_SIMS=10
-RISK_H=5        # seconds
+RISK_H=10        # seconds
 RISK_STEP=0.25   # seconds
 COL_TOL_X=1.0    # meters
 COL_TOL_Y=1.5    # meters
